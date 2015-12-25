496fb186aebc3087b8b82aa98240f7f45fe04d15
kongsiyu <boil@vip.qq.com>
2015-12-25 10:29:41 +0800

    remove the model layer

--- dcim-modeling-web/src/js/service/user.js
+++ dcim-modeling-web/src/js/service/user.js
@@ -3,15 +3,15 @@
  */
 define([
     "main",
-    "model/user",
-    "model/organization",
     "util/string",
     "util/array"
-], function (App, userModel, organizationModel, stringUtil, arrayUtil) {
+], function (App, stringUtil, arrayUtil) {
+	
+	var basePath;
+	var organizationPath = '/organization';
+	var userPath = '/user';
     App.Services.User = function () {
-        /*userModel = new App.Models.User();
-         stringUtil = new App.Utils.StringUtil();
-         arrayUtil = new App.Utils.ArrayUtil();*/
+		basePath = App.Constants.BASE_PATH;
     };
 
     App.Services.User.prototype = {
@@ -25,40 +25,86 @@ define([
 		    	}
 		    }
     	},
-        login: function(username, password, callback) {
+        login: function(username, password, success) {
 			password = stringUtil.md5(password);
-			userModel.login(username, password, function(result) {
-				if (result.header.success) {
-					var user = result.body;
-					window.sessionStorage.setItem(App.Constants.SESSION_USER_KEY, JSON.stringify(user));
+			$.ajax({
+				type: "POST",
+				url: basePath + userPath + "/login",
+				data: {
+					username: username,
+					password: password
+				},
+                dataType: 'json',
+				success: function(result){
+					if (result.header.success) {
+						var user = result.body;
+						window.sessionStorage.setItem(App.Constants.SESSION_USER_KEY, JSON.stringify(user));
+					}
+					success(result);
 				}
-				callback(result);
 			});
 		},
 		logoff: function(){
 			window.sessionStorage.removeItem(App.Constants.SESSION_USER_KEY);
 		},
+		userPage: function (organizationId, name, success) {
+            $.ajax({
+				type: "GET",
+				url: basePath + userPath + "/list",
+				data: {
+					organizationId: organizationId,
+					name: name
+				},
+				dataType: "json",
+				success: success
+			});
+        },
+		addUser: function(user, success) {
+			$.ajax({
+				type: "POST",
+				url: basePath + userPath + "/",
+				data: JSON.stringify(user),
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+				success: success
+			});
+		},
 		organizationTree: function(success) {
-			organizationModel.list(function(result){
+			this.organizationList(function(result){
 				if (result.header.success) {
 					var list = result.body;
 					result.body = arrayUtil.toTree(list, {parentKey:'parent.id'});
 				}
 				success(result);
 			});
+		},
+		organizationList: function(success) {
+			$.ajax({
+				type: "GET",
+				url: basePath + organizationPath + "/list",
+				dataType: "json",
+				success: success
+			});
+		},
+		addOrganization: function(organization, success) {
+			$.ajax({
+				type: "POST",
+				url: basePath + organizationPath + "/",
+				data: JSON.stringify(organization),
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+				success: success
+			});
+		},
+		organizationUsers: function(organizationId, success) {
+			$.ajax({
+				type: "GET",
+				url: basePath + organizationPath + "/" + organizationId + '/users',
+				dataType: "json",
+				success: success
+			});
 		}
     };
-    
-	function toTree(array, parentId) {
-		var list = arrayUtil.findBy(array, [{
-			key: 'org.id',
-			value: orgId
-		}]);
-		for (var i = 0; i < list.length; i++) {
-			list.children = toTree(array, list[i].id);
-		}
-		return list;
-	}
     App.Services.User.prototype.constructor = App.Services.User;
     return new App.Services.User();
 });
